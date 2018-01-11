6e255f970c4d4325abaf372ab301d673b27f4471
Johannes Witt <johawitt@outlook.de>
2018-01-11 20:30:21 +0100

    added message bus unit test

--- UnitTest/message bus.cpp
+++ UnitTest/message bus.cpp
@@ -8,29 +8,17 @@ namespace UnitTest
 {		
 	TEST_CLASS(MessageBus) {
 	public:
-
-		void reviever(pc::mb::Message message) {
-			Assert::AreEqual("Hello World!", static_cast<const char*>(message.data.at(1)));
-		}
 		
 		TEST_METHOD(sendAndRecieve) {
 			pc::mb::Message message;
 			std::string test_string = "Hello World!";
 			message.data.push_back(static_cast<const void*>(test_string.data()));
-			message.type = pc::mb::MessageType::Tree;
+			message.type = pc::mb::MessageType::sfEvent;
 			std::function<void(pc::mb::Message)> f_reciever = [](pc::mb::Message message) {
-				Assert::AreEqual("Hello World!", static_cast<const char*>(message.data.at(1)));
+				Assert::AreEqual("Hello World!", static_cast<const char*>(message.data.at(0)));
 			};
 			bus.subscribe(pc::mb::MessageType::Tree, f_reciever);
 			bus.send(message);
-		}
-
-		TEST_METHOD(subscribe) {
-
-		}
-
-		TEST_METHOD(unsubscribe) {
-
 		}
 
 	private:
