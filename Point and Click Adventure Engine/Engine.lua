b5b4c43a8f3fd63bb2defeb32d8720f9a25c0ae9
HaSa1002 <johawitt@outlook.de>
2018-04-02 02:14:50 +0200

    added Engine.lua as it is always needed to start the engine. It give's a nice overview of what's possible

---
+++ Point and Click Adventure Engine/Engine.lua
@@ -0,0 +1,23 @@
+-- This is the config file for your game
+-- Currently you have to manually register all rooms. This may get obsolete someday
+-- Load our helpers
+-- dofile "helpers.lua"
+
+-- Firstly we allways need to have the rooms table initialized. This may be done in short time by the engine directly
+rooms = {}
+
+-- Don't forget the Localisations
+localisations = {}
+
+-- Register the language files
+-- dofile "locals/en.lang"
+-- dofile "locals/de.lang"
+
+-- Create the Game Table with all Settings etc.
+game = {}
+game.stdlang = "en"
+-- Load the user configs
+-- dofile "Game.conf"
+
+-- Register the rooms
+-- dofile "rooms/lua_testroom.room"
