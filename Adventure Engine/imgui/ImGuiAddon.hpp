3946c7a8e1c68ef0fe79784eb0e6125187c6cff6
HaSa1002 <johawitt@outlook.de>
2018-09-20 18:34:24 +0200

    copied old rendering module changed eventhandling added imgui v1.65 added Engine class

---
+++ Adventure Engine/imgui/ImGuiAddon.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include <vector>
+namespace ImGui {
+	bool Combo(const char* label, int* currIndex, std::vector<std::string>& values);
+	bool ListBox(const char* label, int* currIndex, std::vector<std::string>& values);
+	bool InputText(const char* label, std::string& str, size_t maxInputSize,
+		ImGuiInputTextFlags flags = 0, ImGuiTextEditCallback callback = 0, void* user_data = 0);
+	bool InputTextMultiline(const char* label, std::string& str, size_t maxInputSize, const ImVec2& size = ImVec2(0,0),
+		ImGuiInputTextFlags flags = 0, ImGuiTextEditCallback callback = 0, void* user_data = 0);
+
+	void displayFilterUsage();
+
+}
