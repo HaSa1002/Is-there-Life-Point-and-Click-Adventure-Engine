9a6d637dd0dedf2d552e720821be8b6e3b9cf86b
HaSa1002 <johawitt@outlook.de>
2018-10-22 20:24:35 +0200

    fixed subscription type addition fixed rendering fixed subscribe fixed clearSubscriptions

--- Adventure Engine/engine/EventHandling/EventHandler.lua
+++ Adventure Engine/engine/EventHandling/EventHandler.lua
@@ -1,14 +1,16 @@
 local class = require "middleclass"
 
 
+
 EventHandler = class('EventHandler');
 
 function EventHandler:initialize()
 	self.events = nil
 	self.subscribers = {}
-	for type, v in pairs(EventType) do
+	self.subscribers["update"] = {}
+	self.subscribers["draw"] = {}
+	for type in pairs(getmetatable(EventType).__index) do
 		self.subscribers[type] = {}
-		print(type .. " | " .. v);
 	end
 end
 
@@ -30,14 +32,29 @@ function EventHandler:update(dt)
 	end
 end
 
-function EventHandler:render(target, states)
-	for s in self.subscribers["draw"] do
-		s:call{event.target = target, event.states = states}
+function EventHandler:render(target_, states_)
+	local event = {
+		states = states_,
+		target = target_}
+	for n, s in pairs(self.subscribers.draw) do
+		s:call(event)
 	end
 end
 
 function EventHandler:subscribe(subscription)
-	self.subscribers[subscription.eventType].insert(subscription)
+	local t = self.subscribers[subscription.eventType]
+	if self.subscribers[subscription.eventType] == nil then
+		self.subscribers[subscription.eventType] = {subscription}
+	else self.subscribers[subscription.eventType][#t+1] = subscription
+	end
+end
+
+function EventHandler:clearSubscriptions()
+	self.subscribers.draw = {}
+	self.subscribers.update = {}
+	for type in pairs(getmetatable(EventType).__index) do
+		self.subscribers[type] = {}
+	end
 end
 
 return EventHandler()
