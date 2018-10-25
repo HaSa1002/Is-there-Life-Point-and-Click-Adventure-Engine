ec793791612dec1ad886613bfad7394d978fe18a
HaSa1002 <johawitt@outlook.de>
2018-10-26 00:42:32 +0200

    Same as before

--- Adventure Engine/engine/EventHandling/EventHandler.lua
+++ Adventure Engine/engine/EventHandling/EventHandler.lua
@@ -20,13 +20,10 @@ function EventHandler:setEvents(eventVector)
 end
 
 function EventHandler:handleEvents()
-	local i = require "inspect"
-	--print(i(#self.events))
 	local e = self.events
 	for v=1, #e do
 		local type = e[v].type
-		--print(i(type))
-		for n, s in pairs(self.subscribers[type]) do
+		for num, s in pairs(self.subscribers[type]) do
 			s:call(e[v])
 		end
 	end
@@ -34,7 +31,7 @@ function EventHandler:handleEvents()
 end
 
 function EventHandler:update(dt)
-	for n, s in pairs(self.subscribers.update) do
+	for num, s in pairs(self.subscribers.update) do
 		s:call(dt)
 	end
 end
@@ -43,25 +40,41 @@ function EventHandler:render(target_, states_)
 	local event = {
 		states = states_,
 		target = target_}
-	for n, s in pairs(self.subscribers.draw) do
-		s:call(event)
+		local drawlist = self.subscribers.draw
+		--print(#drawlist)
+	for i, v in pairs(drawlist) do
+		for num, s in pairs(drawlist[i]) do
+			s:call(event)
+		end
 	end
 end
 
 function EventHandler:subscribe(subscription)
-	local t = self.subscribers[subscription.eventType]
-	if self.subscribers[subscription.eventType] == nil then
-		self.subscribers[subscription.eventType] = {subscription}
-	else self.subscribers[subscription.eventType][#t+1] = subscription
+	if subscription.eventType == "draw" then
+		if subscription.layer == nil or subscription.layer < 1 then
+			subscription.layer = 1
+		end
+		local d = self.subscribers.draw[subscription.layer]
+		if  d == nil then
+			self.subscribers.draw[subscription.layer] = {subscription}
+		else 
+			self.subscribers.draw[subscription.layer][#d+1] = subscription
+		end
+	else
+		local t = self.subscribers[subscription.eventType]
+		if t == nil then
+			self.subscribers[subscription.eventType] = {subscription}
+		else 
+			self.subscribers[subscription.eventType][#t+1] = subscription
+		end
 	end
 end
 
 function EventHandler:clearSubscriptions()
 	self.subscribers.draw = {}
 	self.subscribers.update = {}
-	for type in pairs(getmetatable(EventType).__index) do
-		self.subscribers[type] = {}
+	for type, v in pairs(getmetatable(EventType).__index) do
+		self.subscribers[v] = {}
 	end
 end
 
-return EventHandler()
